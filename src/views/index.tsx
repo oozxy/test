8f5405bd5b715473d8f4da189155ca5cdfd45389
polly-zou <310663565@qq.com>
2019-10-02 17:33:01 +0800

    test

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 const Index: React.FC = () => {
   return (
     <div className="home">首页 <span>123123</span>
-    
+      <div className="banner">
+        <p className="title">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </p>
+      </div>
     </div>
   )
 }
