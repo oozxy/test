4bf387c4e5cb39de4ce9c376600d27993fdb9920
Polly Zou <polly.z@tc-gaming.co>
2019-10-04 10:04:05 +0800

    test

---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+import commonReducer from "./common/reducers";
+import lifeVlogReducer from "./lifeVlog/reducers";
+const rootReducer = {
+  commonReducer,
+  lifeVlogReducer
+}
+module.exports=rootReducer
