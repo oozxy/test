e1d21b4712c6dabb72781367cb133ed956df83b0
polly-zou <310663565@qq.com>
2019-10-04 10:04:05 +0800

    test

--- src/store/common/actions.ts
+++ src/store/common/actions.ts
@@ -2,10 +2,7 @@
 import { createAction } from 'redux-actions';
 import { Message } from './models'
 import { SET_VALUE } from "../types"
-const set_value = createAction(
+export const setValue = createAction(
   SET_VALUE,
   (newMessage: Message) => ({newMessage})
 );
-export {
-  set_value
-}
