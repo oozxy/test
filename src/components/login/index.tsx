44f2b1765b0d86142331870c82eec1020ad68fc1
polly-zou <310663565@qq.com>
2019-10-07 11:57:10 +0800

    test

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const Login: React.FC = () => {
   return (
-    <div className="home">登录 components<span>111</span>
+    <div className="home">登录 components<span>222</span>
       <div className="banner">
         <p className="title">
           <ul></ul>
