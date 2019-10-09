4995fc304f0b4663ac61222fa39a03c25501649c
polly-zou <310663565@qq.com>
2019-10-09 17:27:28 +0800

    test

--- file.js
+++ file.js
@@ -10,19 +10,29 @@ function readData(filename){
   // eslint-disable-next-line no-unused-vars
   let akFileData = fs.readFileSync(akFilepath,'utf-8');
   akFileData = JSON.parse(akFileData.toString());
-  const allKey = Object.keys(wt2FileData);
-  for (const key in akFileData) {
-    if(allKey.indexOf(key) < -1){
-      delete akFileData[key];
+  // const allKey = Object.keys(wt2FileData);
+  // for (const key in akFileData) {
+  //   if(allKey.indexOf(key) <= -1){
+  //     delete akFileData[key];
+  //   }
+  // }
+  // fs.writeFile("./commoLang/"+filename,JSON.stringify(akFileData),function(error){
+  //   if(error){
+  //     console.warn(error)
+  //   }
+  // })
+  for (const key in wt2FileData){
+    if(akFileData.hasOwnProperty(key)){
+      wt2FileData[key] = akFileData[key];
     }
   }
-  fs.writeFile("./commoLang/"+filename,JSON.stringify(akFileData),function(error){
+  fs.writeFile("./wt2Lang/"+filename,JSON.stringify(wt2FileData),function(error){
     if(error){
       console.warn(error)
     }
   })
 }
-
+// readData("CN.json")
 fs.readdir(filePath,function(err,files){
   if(err){
     console.warn(err)
