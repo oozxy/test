11fb4f582f0c5b71a31ceb6023c6679b322219d1
time <310663565@qq.com>
2019-10-22 22:46:44 +0800

    test

--- src/store/common/sagas.ts
+++ src/store/common/sagas.ts
@@ -1,1 +1,17 @@
-import { all, call, fork, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { call, put, takeEvery } from 'redux-saga/effects'
+// import Api from '...'
+
+function* getData(action) {
+   try {
+      const response = yield call(api.getData, action.payload.id);
+      yield put({type: "GET_DATA_SUCCEEDED", payload: response});
+   } catch (e) {
+      yield put({type: "GET_DATA_FAILED", payload: error});
+   }
+}
+
+function* mySaga() {
+  yield* takeEvery("GET_DATA", getData);
+}
+
+export default mySaga;
