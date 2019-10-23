55e98daa9e12f2d5740cc1906f9f85d70e288f99
time <310663565@qq.com>
2019-10-23 22:04:59 +0800

    Generator

--- src/store/common/sagas.ts
+++ src/store/common/sagas.ts
@@ -1,17 +1,12 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
 // import Api from '...'
-
-function* getData(action) {
-   try {
-      const response = yield call(api.getData, action.payload.id);
-      yield put({type: "GET_DATA_SUCCEEDED", payload: response});
-   } catch (e) {
-      yield put({type: "GET_DATA_FAILED", payload: error});
-   }
+// Generator函数：可以暂停执行，所以前面要加*号与普通函数以示区别
+// 整个 Generator 函数就是一个封装的异步任务，或者说是异步任务的容器。异步操作需要暂停的地方，都用 yield 语句注明。
+// Generator 函数可以暂停执行和恢复执行
+function* gen(x:number){
+    var y = yield x+2;
+    return y
 }
-
-function* mySaga() {
-  yield* takeEvery("GET_DATA", getData);
-}
-
-export default mySaga;
+var g = gen(1);
+g.next() // { value: 3, done: false }
+g.next() // { value: undefined, done: true }
