551db2eaedcb8f1c58f959cdb281c958c4b3bbab
polly-zou <310663565@qq.com>
2019-10-25 10:19:34 +0800

    test

--- src/store/common/sagas.ts
+++ src/store/common/sagas.ts
@@ -1,4 +1,4 @@
-import { call, put, takeEvery } from 'redux-saga/effects'
+// import { call, put, takeEvery } from 'redux-saga/effects'
 // import Api from '...'
 // Generator函数：可以暂停执行，所以前面要加*号与普通函数以示区别
 // 整个 Generator 函数就是一个封装的异步任务，或者说是异步任务的容器。异步操作需要暂停的地方，都用 yield 语句注明。
