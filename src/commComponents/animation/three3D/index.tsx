bff1e201fe4ce37bb13857734dad51155032d513
time <310663565@qq.com>
2019-10-28 23:25:46 +0800

    test

---
+++ src/commComponents/animation/three3D/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.less"
+interface Three3DProps {
+}
+interface Three3DState {
+}
+class Three3D extends React.Component<Three3DProps,Three3DState>{
+  constructor(props:Three3DProps){
+    super(props);
+  }
+  public render(){
+    return (
+      <div className="three3D_main">
+
+      </div>
+    )
+  }
+}
+export default Three3D
