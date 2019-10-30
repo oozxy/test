eb27611bfd5e45103205a6597012cacd17b72dcb
time <310663565@qq.com>
2019-10-30 21:47:59 +0800

    test

--- src/commComponents/animation/three3D/index.tsx
+++ src/commComponents/animation/three3D/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as THREE from "three";
 import "./index.less";
-interface Three3DProps {}
+interface Three3DProps { }
 interface Three3DState {
   testDom: any;
 }
@@ -54,11 +54,27 @@ class Three3D extends React.Component<Three3DProps, Three3DState> {
     // 渲染，即摄像头拍下此刻的场景
     renderer.render(scene, camera);
   }
-  animate(){
-    
+  test() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
   }
   componentDidMount() {
-    this.initThree();
+    this.test();
   }
   public render() {
     return <div className="three3D_main"></div>;
